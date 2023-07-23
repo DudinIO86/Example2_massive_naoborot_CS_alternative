c508cae73e1681cd677cd9824601151af25cb96c
Igor Dudin <dudinio@mail.ru>
2023-07-23 11:27:37 +0300

    Рабочий код

--- Program.cs
+++ Program.cs
@@ -1,3 +1,36 @@
-﻿int a=11;
+﻿//исходные данные
+int []arrow={0,1,2,3,4,5};
+int size=arrow.Length;
+int index=0;
+int t1=0;
+int t2=size/2;
 
-Console.WriteLine(a/2);
+//вывод на печать исходного массива
+var str=string.Join(" ",arrow);
+Console.WriteLine(str);
+
+//блок операций
+if (size%2==0)
+{
+    while(index<size/2)
+    {
+        t1=arrow[size-index-1];
+        arrow[size-index-1]=arrow[index];
+        arrow[index]=t1;
+        index++;
+    }
+}
+else
+{
+    while(index<t2)
+    {
+        t1=arrow[size-index-1];
+        arrow[size-index-1]=arrow[index];
+        arrow[index]=t1;
+        index++;
+    }
+}
+
+//вывод на печать преобразованного массива
+var str2=string.Join(" ",arrow);
+Console.WriteLine(str2);
